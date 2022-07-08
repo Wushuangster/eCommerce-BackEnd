1531e3e69e7f9ec0bde0f19378b53a10d9c50612
wushuangster <wushuangster@hotmail.com>
2022-07-07 23:09:59 -0700

    updated the four models

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Products belongToMany Tags (through ProductTag) // Using the ProductTag through model
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag) // Using the ProductTag through model
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag
+};
